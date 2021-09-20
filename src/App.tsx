d36b77f4597b4cd13e5c20fec95b331f0dd33f7c
dennyparas <dennyparas28@gmail.com>
2021-09-21 02:51:03 +0800

    add edamam attribution in the footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,33 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { RecipesProvider } from "./context/recipeContext";
 import Favorites from "./pages/Favorites";
 import Compare from "./pages/Compare";
-import {} from "react-router-dom";
+import Footer from "./components/Footer";
+import "./App.css";
 
 const App: React.FC<{}> = () => {
   return (
     <RecipesProvider>
       <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/recipe/:recipe_id">
-            <Recipe />
-          </Route>
-          <Route exact path="/favorites">
-            <Favorites />
-          </Route>
-          <Route exact path="/compare">
-            <Compare />
-          </Route>
-        </Switch>
+        <div className="App">
+          <Navbar />
+          <div className="App-content">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/recipe/:recipe_id">
+                <Recipe />
+              </Route>
+              <Route exact path="/favorites">
+                <Favorites />
+              </Route>
+              <Route exact path="/compare">
+                <Compare />
+              </Route>
+            </Switch>
+          </div>
+          <Footer></Footer>
+        </div>
       </Router>
     </RecipesProvider>
   );
